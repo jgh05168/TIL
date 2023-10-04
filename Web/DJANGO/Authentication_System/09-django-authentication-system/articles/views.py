35607c6b503a704fe14ba1cf630ccdeaf82395db
gyuhunJeon <jgh05168@gmail.com>
2023-10-04 14:04:02 +0900

    1004

--- Web/DJANGO/Authentication_System/09-django-authentication-system/articles/views.py
+++ Web/DJANGO/Authentication_System/09-django-authentication-system/articles/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render, redirect
 from .models import Article
 from .forms import ArticleForm
 
+from django.contrib.auth.decorators import login_required       # 회원 인증
+
 
 # Create your views here.
 def index(request):
@@ -20,7 +22,13 @@ def detail(request, pk):
     return render(request, 'articles/detail.html', context)
 
 
+@login_required         # 로그인 한 사람만 create를 실행할 수 있도록 데코레이터 설정(lock을 걸어주는 역할)
+# 로그인을 하지 않은 상태로 create로 접근한다면 login 창으로 넘어간다.
 def create(request):
+    # 데코레이터를 사용하지 않는 경우
+    if not request.user.is_authenticated:       # 인증되지 않은 유저가 create에 접근한다면
+        return redirect('accounts:login')       # 로그인 창으로 보내버리기
+
     if request.method == 'POST':
         form = ArticleForm(request.POST)
         if form.is_valid():
