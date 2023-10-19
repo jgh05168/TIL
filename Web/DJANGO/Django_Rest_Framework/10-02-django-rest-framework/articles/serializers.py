daa0f38787db7c867475e477a4b5316236454e20
gyuhunJeon <jgh05168@gmail.com>
2023-10-19 11:03:46 +0900

    1019

--- Web/DJANGO/Django_Rest_Framework/10-02-django-rest-framework/articles/serializers.py
+++ Web/DJANGO/Django_Rest_Framework/10-02-django-rest-framework/articles/serializers.py
@@ -1,5 +1,5 @@
 from rest_framework import serializers
-from .models import Article
+from .models import Article, Comment
 
 
 class ArticleListSerializer(serializers.ModelSerializer):
@@ -8,7 +8,30 @@ class ArticleListSerializer(serializers.ModelSerializer):
         fields = ('id', 'title', 'content',)
 
 
+class CommentSerializer(serializers.ModelSerializer):
+    # 내부에서만 사용가능한 새로운 article 클래스 지정(title 정보만 사용하고 싶으므로)
+    class ArticleSerializer(serializers.ModelSerializer):
+        class Meta:
+            model = Article
+            fields = ('title',) 
+
+    # class override
+    article = ArticleSerializer(read_only=True)     # 읽기 전용 필드를 True로 설정(Meta의 read_only_field와 중복이 되기 때문에 여기서 설정)
+
+    class Meta:
+        model = Comment
+        fields = '__all__'
+        # read_only_fields = ('article',)         # 읽기 전용 필드
+
+
+
 class ArticleSerializer(serializers.ModelSerializer):
+    # Nested_relationships
+    # 댓글 목록
+    comment_set = CommentSerializer(many=True, read_only=True)
+    # 댓글 개수
+    # 새로운 serializerField 사용
+    comment_count = serializers.IntegerField(source='comment_set.count', read_only=True)
     class Meta:
         model = Article
         fields = '__all__'
--- Web/DJANGO/Django_Rest_Framework/10-02-django-rest-framework/articles/views.py
+++ Web/DJANGO/Django_Rest_Framework/10-02-django-rest-framework/articles/views.py
@@ -1,9 +1,9 @@
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
 from rest_framework import status
-from .models import Article
-from .serializers import ArticleListSerializer, ArticleSerializer
-
+from .models import Article, Comment
+from .serializers import ArticleListSerializer, ArticleSerializer, CommentSerializer
+from django.shortcuts import get_list_or_404, get_object_or_404
 
 @api_view(['GET', 'POST'])
 def article_list(request):
@@ -38,3 +38,37 @@ def article_detail(request, article_pk):
             serializer.save()
             return Response(serializer.data)
         # return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+@api_view(['GET'])
+def comment_list(request):
+    comments = Comment.objects.all()
+    serializer = CommentSerializer(comments, many=True)
+    return Response(serializer.data)
+
+
+@api_view(['GET', 'DELETE', 'PUT'])
+def comment_detail(request, comment_pk):
+    comment = Comment.objects.get(pk=comment_pk)    # 공통 코드
+    if request.method == 'GET':
+        serializer = CommentSerializer(comment)
+        return Response(serializer.data)
+    
+    elif request.method == 'DELETE':
+        comment.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+        
+    elif request.method == 'PUT':
+        serializer = CommentSerializer(comment, data=request.data)
+        if serializer.is_valid(raise_exception=True):
+            serializer.save()       # seializer에 comment 정보가 이미 들어가있으므로 인스턴스로 넣어줄 필요가 없음
+            return Response(serializer.data)
+
+
+@api_view(['POST'])
+def comment_create(request, article_pk):
+    article = Article.objects.get(pk=article_pk)
+    serializer = CommentSerializer(data=request.data)
+    if serializer.is_valid(raise_exception=True):
+        serializer.save(article=article)        # article = article : 특정 serializer 인스턴스를 저장하는 과정에서 추가데이터로 들어갈 수 있다.
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
